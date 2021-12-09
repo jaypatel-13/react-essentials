838d159c06112f0a36889ff85ad5dc2b4319a43e
Jay <patel8g3@uwindsor.ca>
2021-12-09 16:54:11 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from "./App";
+
+ReactDOM.render(
+  
+    <App login="jaypatel-13"/>
+    
+  
+    ,
+  document.getElementById('root')
+);
+
